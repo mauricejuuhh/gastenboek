d4dc44a39412af63e48372627c2fedf958217c07
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2017-11-17 23:29:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,35 +10,40 @@
       <textarea name="bericht" rows="8" cols="80"></textarea>
       <input type="submit" name="submit" value="verstuur">
     </form>
+    Het woord dat wordt gefiltert is lol<br>
+    En er is gebruik gemaakt van PDO<br><br>
 <?php
-$dbc = mysqli_connect('localhost','root','root','gastenboek');
-$select = "SELECT naam,bericht FROM berichten";
+$dbc = new PDO('mysql:hosts=localhost;dbname=21960_db', 'maurice', 'depies');
+$select = $dbc->prepare("SELECT naam,bericht FROM gastenboek");
+$select->execute();
 
-$result = mysqli_query($dbc, $select);
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
+
+    while ($row = $select->fetch()) {
 echo '<b>Naam: </b>' . $row["naam"] . '<br>';
 echo '<b>Bericht: </b>' . $row["bericht"] . '<br>';
 echo '_________________________________________________<br>';
     }
-}
-if (isset($_POST["submit"])) {
-  $naam = $_POST["naam"];
-  $bericht = $_POST["bericht"];
 
-  $naamFilter = preg_match("#\b(^kut)\b#", $naam);
-  $berichtFilter = preg_match("#\b(^kut)\b#", $bericht);
-if ($naamFilter && $berichtFilter) {
-  echo "je mag geen kut zeggen";
+if (isset($_POST["submit"])) {
+  $naam = strtolower($_POST["naam"]);
+  $bericht = strtolower($_POST["bericht"]);
+
+  $naamFilter = preg_replace('/\blol\b/', "***", $naam);
+  $berichtFilter = preg_replace('/\blol\b/', "***", $bericht);
+
+if ($naam != $naamFilter || $bericht != $berichtFilter) {
+  echo "je mag geen lol zeggen";
 } else {
-  $insertImage = "INSERT INTO berichten(naam, bericht) VALUES ('$naam', '$bericht')";
-  $result = mysqli_query($dbc, $insertImage);
+  $insertImage = $dbc->prepare("INSERT INTO gastenboek(naam, bericht) VALUES (?,?)");
+  $insertImage->bindParam(1,$naamFilter);
+  $insertImage->bindParam(2,$berichtFilter);
+  $insertImage->execute();
+
   echo "gelukt";
 }
 
 }
 
-mysqli_close($dbc);
  ?>
 
   </body>
